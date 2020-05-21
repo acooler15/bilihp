10359ac5dc59ccae93e07822f14e804249c75170
acooler15 <renzongjia@vip.qq.com>
2020-05-22 00:04:36 +0800

    add display Release time

--- luci-app-bilihp/luasrc/controller/bilihp.lua
+++ luci-app-bilihp/luasrc/controller/bilihp.lua
@@ -10,6 +10,7 @@ function index()
 function act_status()
 	local e={}
 	e.running=luci.sys.call("busybox ps -w | grep bilihp_router | grep -v grep >/dev/null")==0
+	e.timestamp=luci.sys.exec("busybox cat /tmp/bilihp/timestamp")
 	luci.http.prepare_content("application/json")
 	luci.http.write_json(e)
 	end
